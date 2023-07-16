b9b91f2a3944dd083025af67201bb8bbc56e9f04
herb <69900670+WhiteHerb@users.noreply.github.com>
2023-07-16 22:40:23 +0900

    Update CheckHateSpeech.py

--- CheckHateSpeech.py
+++ CheckHateSpeech.py
@@ -67,6 +67,9 @@ with open('./CheckHateSpeech/t2i.json', 'r', encoding='utf-8') as f:
 with open('./CheckHateSpeech/i2t.json', 'r', encoding='utf-8') as f:
     i2t = json.load(f)
 
+import os
+print(os.listdir())
+
 model = load_model('/CheckHateSpeech/CheckHateSpeech.h5')
 
 
